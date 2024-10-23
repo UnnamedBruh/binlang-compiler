baf958a2f238677b65b7044d8fa1557cf8e7094a
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-10-23 12:01:11 -0700

    Update newcompiler.js

--- src/newcompiler.js
+++ src/newcompiler.js
@@ -1,6 +1,7 @@
 const BINLangCompilerNew = function(code, ret = "arraybuffer") {
-	const array = [1], tokens = code.match(/\[[A-Z0-9]+\]|[a-zA-Z]+|-?[0-9]+(\.[0-9]*)?|[^ \t]/gms);
+	const array = [1], tokens = code.match(/(?:\[([A-Z0-9]+)\])|[a-zA-Z]+|-?[0-9]+(\.[0-9]*)?|[^ \t]/gms);
 	const len = tokens.length >>> 0, zero = 0 >>> 0, one = 1 >>> 0, two = 2 >>> 0, three = 3 >>> 0, four = 4 >>> 0, five = 5 >>> 0, eight = 8 >>> 0, tff = 255 >>> 0, tfs = 256 >>> 0, note = -128, ote = 128 >>> 0;
+	const typeOrder = {"UINT8":zero,"UINT16":one,"INT8":two,"UFLOAT16":three};
 	let state = zero, token, lineCount = zero, identifiers = {}, amountOfIdentifiers = zero, substate = zero;
 	function compress(ident, newi = false) {
 		if (identifiers[ident]) return identifiers[ident];
@@ -41,10 +42,9 @@ const BINLangCompilerNew = function(code, ret = "arraybuffer") {
 				array.push(...compress(token, true), zero);
 				substate = one;
 			} else if (substate === one) {
-				const type = token.slice(one, -1);
-				const indext = ({"UINT8":zero,"UINT16":one,"INT8":two,"UFLOAT16":three})[type];
+				const indext = typeOrder[token];
 				if (indext === undefined) {
-					throw new TypeError(token + " is not a valid type. The current types available are [UINT8], [UINT16], [INT8], and [UFLOAT16].")
+					throw new TypeError("[" + token + "] is not a valid type. The current types available are [UINT8], [UINT16], [INT8], and [UFLOAT16].")
 				}
 				array.push(indext);
 				substate = (indext + two) >>> zero;
