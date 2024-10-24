410115c69829510f89356c6732e1fc20c2d0de2d
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-10-23 17:31:59 -0700

    Update newcompiler.js

--- src/newcompiler.js
+++ src/newcompiler.js
@@ -1,7 +1,7 @@
 const BINLangCompilerNew = function(code, ret = "arraybuffer") {
 	const array = [1], tokens = code.match(/\[([A-Z0-9]+)\]|"([^"\n\\]|\\(["\\]|[0-9]+|\\[nt]))+"|[a-zA-Z]+|-?[0-9]+(\.[0-9]*)?|[\n;](?:[\n;]*)|[^ \t]/gms);
 	const len = tokens.length >>> 0, zero = 0 >>> 0, one = 1 >>> 0, two = 2 >>> 0, three = 3 >>> 0, four = 4 >>> 0, five = 5 >>> 0, six = 6 >>> 0, seven = 7 >>> 0, eight = 8 >>> 0, nine = 9 >>> 0, tff = 255 >>> 0, tfs = 256 >>> 0, note = -128, ote = 128 >>> 0;
-	const typeOrder = {"UINT8":zero,"UINT16":one,"INT8":two,"UFLOAT16":three,"UTF8STRING":four}, stringEsc = {"n":10>>>zero,"t":nine}, back = "\\";
+	const typeOrder = {"UINT8":zero,"UINT16":one,"INT8":two,"UFLOAT16":three,"UTF8STRING":four}, stringEsc = {"n":10>>>zero,"t":nine,"\\":92>>>zero}, back = "\\";
 	let state = zero, token, lineCount = zero, identifiers = {}, amountOfIdentifiers = zero, substate = zero, valuePassed;
 	function compress(ident, newi = false) {
 		if (identifiers[ident]) return identifiers[ident];
