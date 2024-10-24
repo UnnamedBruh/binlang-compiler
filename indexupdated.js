314367d03b5e3ab0fd4bb3d1cc4c62a2569e55b0
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-10-24 13:01:55 -0700

    Update indexupdated.js

--- indexupdated.js
+++ indexupdated.js
@@ -1,5 +1,8 @@
 document.getElementById("compile").addEventListener("click", () => {
+	const time = performance.now()
 	const program = BINLangCompilerNew(document.getElementById("program").value, "arraybuffer")
+	const t2 = performance.now()
+	document.getElementById("timetook").textContent = "Time took: " + (t2 - time) + " ms"
 	const blob = new Blob([program], {type: "application/octet-stream"})
 	const bu = URL.createObjectURL(blob)
 	const a = document.createElement("a")
