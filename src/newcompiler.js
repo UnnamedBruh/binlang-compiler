bb94687bc07453e39b73df6f3f95b67fc0997577
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-10-24 12:29:07 -0700

    Update newcompiler.js

--- src/newcompiler.js
+++ src/newcompiler.js
@@ -1,6 +1,6 @@
 const BINLangCompilerNew = function(code, ret = "arraybuffer") {
 	const array = [1 >>> 0], tokens = code.match(/\[[A-Z0-9]+\]|"([^"\n\\]|\\([0-9]+|[^ 0-9\n\t]))+"|[a-zA-Z_]+|-?[0-9]+(\.[0-9]*)?|[\n;](?:[\n;]*)|[^ \t]/gms);
-	const len = tokens.length >>> 0, zero = 0 >>> 0, one = 1 >>> 0, two = 2 >>> 0, three = 3 >>> 0, four = 4 >>> 0, five = 5 >>> 0, six = 6 >>> 0, seven = 7 >>> 0, eight = 8 >>> 0, nine = 9 >>> 0, ten = 10 >>> 0, tff = 255 >>> 0, tfs = 256 >>> 0, note = -128, ote = 128 >>> 0;
+	const len = tokens.length >>> 0, zero = 0 >>> 0, one = 1 >>> 0, two = 2 >>> 0, three = 3 >>> 0, four = 4 >>> 0, five = 5 >>> 0, six = 6 >>> 0, seven = 7 >>> 0, eight = 8 >>> 0, nine = 9 >>> 0, ten = 10 >>> 0, tff = 255 >>> 0, tfs = 256 >>> 0, note = -128, ote = 128 >>> 0, st = 65536 >>> 0;
 	const typeOrder = {"UINT8":zero,"UINT16":one,"INT8":two,"UFLOAT16":three,"UTF8STRING":four,"BOOLEAN":ten}, stringEsc = {"n":ten,"t":nine,"\\":92>>>zero}, back = "\\", msgWarn = {"0": "Ending the string using a nullish character is NOT recommended! You should use the end of the string literal instead!", "65536": "This character (CHAR) cannot be escaped yet. Since this issue occurred, the unexpected escape sequence will be replaced with a null character to terminate the string."},
 		integ = {"STANDARD":zero,"INTEGER":one,"NONE":two};
 	let state = zero, token, lineCount = zero, identifiers = {}, amountOfIdentifiers = zero, substate = zero, valuePassed;
@@ -8,7 +8,7 @@ const BINLangCompilerNew = function(code, ret = "arraybuffer") {
 		if (identifiers[ident]) return identifiers[ident];
 		const ea = ((amountOfIdentifiers >> eight) + one) >>> zero, array = [];
 		let id = amountOfIdentifiers;
-		for (let i = 0; i !== ea; i++, i >>>= zero) {
+		for (let i = zero; i !== ea; i++, i >>>= zero) {
 			array.push(Math.min(tff, id));
 			id -= tfs;
 		}
@@ -51,7 +51,7 @@ const BINLangCompilerNew = function(code, ret = "arraybuffer") {
 				array.push(...compress(token, true), zero);
 				substate = one;
 			} else if (substate === one) {
-				if (token[0] !== "[" || token[token.length - one] !== "]") {
+				if (token[zero] !== "[" || token[token.length - one] !== "]") {
 					throw new SyntaxError("The type has to be bracketed to signify that '" + token + "' is a proper type.")
 				}
 				const indext = typeOrder[token.slice(one, -1)];
@@ -80,13 +80,13 @@ const BINLangCompilerNew = function(code, ret = "arraybuffer") {
 			} else if (substate === six) {
 				const dec = token.slice(one, -1);
 				const len = dec.length;
-				let char = 0, end = true;
-				if (valuePassed === 0) {
+				let char = zero, end = true;
+				if (valuePassed === zero) {
 					let i = zero;
 					for (;i < len; i++, i >>>= zero) {
-						char = (dec[i] === back ? stringEsc[dec[i++ + one] || 65536] : dec.charCodeAt(i)) >>> zero;
+						char = (dec[i] === back ? stringEsc[dec[i++ + one] || st] : dec.charCodeAt(i)) >>> zero;
 						if (char > tff) throw new TypeError("Found a character outside of the UTF8 range: '" + dec[i] + "'. If you need to use a character outside of the UTF8 range, please use the [UTF16STRING] type.");
-						if (char === zero || char === 65536) {
+						if (char === zero || char === st) {
 							console.warn(char === zero ? msgWarn[char] : msgWarn[char].replace(/CHAR/, dec[i]));
 							array.push(char);
 							end = false;
@@ -94,12 +94,12 @@ const BINLangCompilerNew = function(code, ret = "arraybuffer") {
 						}
 						array.push(char);
 					}
-				} else if (valuePassed === 1) {
+				} else if (valuePassed === one) {
 					let j = zero;
 					for (;j < len; j++, j >>>= zero) {
-						char = (dec[j] === back ? 65536 : dec.charCodeAt(j)) >>> 0;
-						if (char === 65536) {
-							if (isNaN(Number(dec[j + 1]))) {
+						char = (dec[j] === back ? st : dec.charCodeAt(j)) >>> zero;
+						if (char === st) {
+							if (isNaN(Number(dec[j + one]))) {
 								console.warn("An empty escaper can be removed from the string entirely, since there is no character to represent");
 							} else {
 								j++;
@@ -124,7 +124,7 @@ const BINLangCompilerNew = function(code, ret = "arraybuffer") {
 					}
 				} else {
 					for (let j = zero; j < len; j++, j >>>= zero) {
-						char = dec.charCodeAt(j) >>> 0;
+						char = dec.charCodeAt(j) >>> zero;
 						if (char > tff) throw new TypeError("Found a character outside of the UTF8 range: '" + dec[j] + "'. If you need to use a character outside of the UTF8 range, please use the [UTF16STRING] type.");
 						array.push(char);
 					}
@@ -154,14 +154,14 @@ const BINLangCompilerNew = function(code, ret = "arraybuffer") {
 				i++;
 			}
 			state = zero;
-			lineCount++, lineCount >>>= 0;
+			lineCount++, lineCount >>>= zero;
 		} else if (state === three) {
 			while (token[zero] !== "\n" && i !== len) {	
 				token = tokens[i];
 				i++;
 			}
 			state = zero;
-			lineCount++, lineCount >>>= 0;
+			lineCount++, lineCount >>>= zero;
 		}
 	}
 	if (state !== zero || substate !== zero) {
