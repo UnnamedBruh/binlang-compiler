ef4abcaf322793a47b692136b1467d34cf2411d8
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-10-23 22:21:50 -0700

    Update newcompiler.js

--- src/newcompiler.js
+++ src/newcompiler.js
@@ -1,7 +1,8 @@
 const BINLangCompilerNew = function(code, ret = "arraybuffer") {
 	const array = [1 >>> 0], tokens = code.match(/\[[A-Z0-9]+\]|"([^"\n\\]|\\([0-9]+|[^ 0-9\n\t]))+"|[a-zA-Z]+|-?[0-9]+(\.[0-9]*)?|[\n;](?:[\n;]*)|[^ \t]/gms);
 	const len = tokens.length >>> 0, zero = 0 >>> 0, one = 1 >>> 0, two = 2 >>> 0, three = 3 >>> 0, four = 4 >>> 0, five = 5 >>> 0, six = 6 >>> 0, seven = 7 >>> 0, eight = 8 >>> 0, nine = 9 >>> 0, ten = 10 >>> 0, tff = 255 >>> 0, tfs = 256 >>> 0, note = -128, ote = 128 >>> 0;
-	const typeOrder = {"UINT8":zero,"UINT16":one,"INT8":two,"UFLOAT16":three,"UTF8STRING":four,"BOOLEAN":ten}, stringEsc = {"n":10>>>zero,"t":nine,"\\":92>>>zero}, back = "\\", msgWarn = {"0": "Ending the string using a nullish character is NOT recommended! You should use the end of the string literal instead!", "65536": "This character (CHAR) cannot be escaped yet. Since this issue occurred, the unexpected escape sequence will be replaced with a null character to terminate the string."};
+	const typeOrder = {"UINT8":zero,"UINT16":one,"INT8":two,"UFLOAT16":three,"UTF8STRING":four,"BOOLEAN":ten}, stringEsc = {"n":10>>>zero,"t":nine,"\\":92>>>zero}, back = "\\", msgWarn = {"0": "Ending the string using a nullish character is NOT recommended! You should use the end of the string literal instead!", "65536": "This character (CHAR) cannot be escaped yet. Since this issue occurred, the unexpected escape sequence will be replaced with a null character to terminate the string."},
+		integ = {"STANDARD":zero,"INTEGER":one,"NONE":two};
 	let state = zero, token, lineCount = zero, identifiers = {}, amountOfIdentifiers = zero, substate = zero, valuePassed;
 	function compress(ident, newi = false) {
 		if (identifiers[ident]) return identifiers[ident];
@@ -76,7 +77,7 @@ const BINLangCompilerNew = function(code, ret = "arraybuffer") {
 				const dec = token.slice(one, -1);
 				const len = dec.length;
 				let char = 0, end = true;
-				if (valuePassed) {
+				if (valuePassed === 0) {
 					let i = zero;
 					for (;i < len; i++, i >>>= zero) {
 						char = (dec[i] === back ? stringEsc[dec[i++ + one] || 65536] : dec.charCodeAt(i)) >>> zero;
@@ -89,7 +90,7 @@ const BINLangCompilerNew = function(code, ret = "arraybuffer") {
 						}
 						array.push(char);
 					}
-				} else {
+				} else if (valuePassed === 1) {
 					let j = zero;
 					for (;j < len; j++, j >>>= zero) {
 						char = (dec[j] === back ? 65536 : dec.charCodeAt(j)) >>> 0;
@@ -117,13 +118,19 @@ const BINLangCompilerNew = function(code, ret = "arraybuffer") {
 						}
 						array.push(char);
 					}
+				} else {
+					for (let j = zero; j < len; j++, j >>>= zero) {
+						char = dec.charCodeAt(j) >>> 0;
+						if (char > tff) throw new TypeError("Found a character outside of the UTF8 range: '" + dec[j] + "'. If you need to use a character outside of the UTF8 range, please use the [UTF16STRING] type.");
+						array.push(char);
+					}
 				}
 				if (end) array.push(zero);
 				state = zero;
 				substate = zero;
 			} else if (substate === eight) {
-				const b = token !== "TRUE";
-				if (b && token !== "FALSE") throw new TypeError("If you want to know how to use [UTF16STRING] or [UTF8STRING], here is how you encountered this error:\nThe type would usually expect a setting that determines whether the string would allow backslash characters on numbers (e.g. \\0, \\1, etc.), and that setting can be rerpesented as either 'TRUE', or 'FALSE', without single quotes, respectively. And lastly\n\nUnexpected token '" + token + "'");
+				const b = integ[token];
+				if (b === undefined) throw new TypeError("If you want to know how to use [UTF16STRING] or [UTF8STRING], here is how you encountered this error:\nThe type would usually expect a setting that determines whether the string would allow backslash characters on numbers (e.g. \\0, \\1, etc.), and that setting can be rerpesented as either 'TRUE', or 'FALSE', without single quotes, respectively. And lastly\n\nUnexpected token '" + token + "'");
 				valuePassed = b;
 				substate -= two;
 			} else if (substate === ten) {
