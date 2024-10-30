cacdec4f4e01abdb7998a8d97abc9a5d31e4d4d1
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-10-30 11:03:53 -0700

    Update indexupdated.js

--- indexupdated.js
+++ indexupdated.js
@@ -4,7 +4,7 @@ document.getElementById("compile").addEventListener("click", () => {
 	const a = performance.now();
 	const p = BINLangCompilerNew(h, x);
 	const b = performance.now();
-	document.getElementById("timetook").textContent = "Time took: " + (t2 - time) + " ms";
+	document.getElementById("timetook").textContent = "Time took: " + (b - a) + " ms";
 	if (document.getElementById("downloadfile").checked) {
 		const c = new Blob([p], {type: "application/octet-stream"});
 		const d = URL.createObjectURL(c);
