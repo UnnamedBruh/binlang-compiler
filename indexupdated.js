deb364266de374e40d3f9ba7a6f14b56687d1ebc
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-10-30 11:10:57 -0700

    Update indexupdated.js

--- indexupdated.js
+++ indexupdated.js
@@ -1,18 +1,22 @@
 const x = "arraybuffer", y = "result.binl";
 document.getElementById("compile").addEventListener("click", () => {
-	const h = document.getElementById("program").value;
-	const a = performance.now();
-	const p = BINLangCompilerNew(h, x);
-	const b = performance.now();
-	document.getElementById("timetook").textContent = "Time took: " + (b - a) + " ms";
-	if (document.getElementById("downloadfile").checked) {
-		const c = new Blob([p], {type: "application/octet-stream"});
-		const d = URL.createObjectURL(c);
-		const a = document.createElement("a");
-		a.href = d;
-		a.download = y;
-		a.click();
-		a.remove();
-		URL.revokeObjectURL(d);
+	const h = document.getElementById("program").value || "";
+	try {
+		const a = performance.now();
+		const p = BINLangCompilerNew(h, x);
+		const b = performance.now();
+		document.getElementById("timetook").textContent = "Time took: " + (b - a) + " ms";
+		if (document.getElementById("downloadfile").checked) {
+			const c = new Blob([p], {type: "application/octet-stream"});
+			const d = URL.createObjectURL(c);
+			const a = document.createElement("a");
+			a.href = d;
+			a.download = y;
+			a.click();
+			a.remove();
+			URL.revokeObjectURL(d);
+		}
+	} catch (error) {
+		alert(error.name + ": " + error.message);
 	}
 })
